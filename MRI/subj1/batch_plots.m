08d001ca82dd37c413b31be3280a039fd82d423e
Andrea AK <aiavenak@gmail.com>
2015-11-06 16:32:11 -0500

    4 PM

--- MRI/subj1/batch_plots.m
+++ MRI/subj1/batch_plots.m
@@ -1,6 +1,6 @@
 close all;
 clear all;
-
+load(fullfile(pwd,'..','..','clrmaps.mat'));
 load connectome.mat;
 
 figure,
@@ -18,7 +18,7 @@ figure, imagesc(Taal_lp'); colormap jet; xlabel('time'); ylabel('regions');
 FC=corr(Taal_lp); % pairwise pearson correllations between every two time series.
 figure, imagesc(FC); axis square; colormap jet; xlabel('regions'); ylabel('regions');
 
-load('../../ordering_matrices/yeo_RS7.mat');
+load('../../ordering_matrices/yeo_RS17.mat');
 
 figure, imagesc(FC(yeoOrder,yeoOrder)); axis square; colormap jet; xlabel('regions'); ylabel('regions');
 title('FC all')
@@ -43,7 +43,24 @@ hist(FCclean(mask),50); axis square;
 subplot(1,3,3);
 plot(FC(mask),FCclean(mask),'ko'); axis square;
 
+C=ones(1200,1);
+R=nan(size(Taal_lp));
+for i=1:N
+    ts=Taal_lp(:,i);
+    [a,b,r]=regress(ts,[avgSignal',C]);
+    R(:,i)=r;
+end
+
+figure, imagesc(R'); colormap jet;
+caxis([-0.8 0.8])
 
 
+
+FCR=corr(R);
+figure, imagesc(FCR(yeoOrder,yeoOrder),[-1,1]); axis square; xlabel('regions'); ylabel('regions');
+title('FC clean')
+colormap(redblue(end:-1:1,:)./255)
+
+figure, hist(FCR(mask),50); axis square;
 
 
