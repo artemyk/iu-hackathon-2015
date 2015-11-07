d48df1145582b57fefae87c36fa66d5040dea433
Peggy Lindenlaub <hpstrn30@hm012.karst.uits.iu.edu>
2015-11-07 13:05:46 -0500

    created the plot

--- MRI/vision_plots.m
+++ MRI/vision_plots.m
@@ -13,20 +13,21 @@ for i =1:3
     % regions to keep
     lregs = [ 222, 205, 207, 200, 199, 192, 178, 175, 184 ];
     rregs = [ 86, 98, 72, 67, 66, 70, 69, 44, 41, 43 ];
+    regs = [ lregs rregs ];
     
     % subset the beginning
-    yeoOrder = yeoOrder([lregs rregs]);
+    % yeoOrder = yeoOrder(regs);
     
     % subset to ROIs in occipital / partietal
-    Taal_lp = Taal_lp(:, [ lregs rregs ]);
+    % Taal_lp = Taal_lp(:, [ lregs rregs ]);
     
     FC=corr(Taal_lp); % pairwise pearson correllations between every two time series.
     
     load('../ordering_matrices/yeo_RS7.mat');
     
     avgSignal = mean(Taal_lp');
-    threshold =1.00;
-    mask = abs(avgSignal)<threshold;
+    threshold = 1.00;
+    mask = abs(avgSignal) < threshold;
     
     nnz(~mask)
     
@@ -47,22 +48,25 @@ for i =1:3
     % caxis([-0.8 0.8])
     
     FCR=corr(R);
-    figure, imagesc(FCR(yeoOrder,yeoOrder),[-1,1]); axis square; xlabel('regions'); ylabel('regions');
+    FCR = FCR(regs, regs); % SUBSET JUST THE REGIONS WE WANT
+    % figure, imagesc(FCR(yeoOrder,yeoOrder),[-1,1]); axis square; xlabel('regions'); ylabel('regions');
+    figure, imagesc(FCR,[-1,1]); axis square; xlabel('region #'); ylabel('region #');
+    ax = gca; ax.XTick = 1:size(regs, 2); ax.YTick = 1:size(regs, 2); ax.XTickLabel = {regs}; ax.YTickLabel = {regs};
     title('FC clean')
     colormap(redblue(end:-1:1,:)./255)
     
     % figure, hist(FCR(mask),50); axis square;
     save(fullfile(pwd,subj,'FC_new.mat'),'FCR');
     
-    SC = M_w;  %(yeoOrder,yeoOrder);
-    ncomm = unique(yeoROIs);
-    for cc1 = 1:length(ncomm)
-        ff = find(yeoROIs == ncomm(cc1));
-        for cc2 = cc1:length(ncomm)
-            gg = find(yeoROIs == ncomm(cc2));
-            rsn_dens(cc1,cc2) = nnz(SC(ff,gg));
-            rsn_dens(cc1,cc2) = rsn_dens(cc1,cc2)./(length(ff)*length(gg));
-        end
-    end
+    %SC = M_w;  %(yeoOrder,yeoOrder);
+    %ncomm = unique(yeoROIs);
+    %for cc1 = 1:length(ncomm)
+    %    ff = find(yeoROIs == ncomm(cc1));
+    %    for cc2 = cc1:length(ncomm)
+    %        gg = find(yeoROIs == ncomm(cc2));
+    %        rsn_dens(cc1,cc2) = nnz(SC(ff,gg));
+    %        rsn_dens(cc1,cc2) = rsn_dens(cc1,cc2)./(length(ff)*length(gg));
+    %    end
+    %end
     
 end
